306295d428248cd7634603b347813d94f43ea993
Aleksandr Samokhin <samokhinteam@gmail.com>
2025-12-12 19:38:24 +0800

    add laravel horizon

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     Usamamuneerchaudhary\Commentify\Providers\CommentifyServiceProvider::class,
 ];
